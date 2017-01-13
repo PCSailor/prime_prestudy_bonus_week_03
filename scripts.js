e93262a3c28cd7b2cb996bdfa3661fbd3f5d3099
PCSailor <phil@philcurtis.io>
2017-01-13 12:17:59 -0600

    initial commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+/*
+function getArea(width, height) {
+return width * height;
+} //Works OK
+*/
+function numOne(a) {
+  return (a * a) / a;
+}
+function numTwo(b) {
+  return (b + b) * (b * b) / b;
+}
+function numThree(c) {
+  return (c % (c * c * c) + (c + c + c));
+}
+function combine(numOne, NumTwo, numThree) {
+  return numOne + NumTwo + numThree;
+}
+/*
+var msg = numOne;
+var func = function() {
+  console.log(msg);
+}
+*/
